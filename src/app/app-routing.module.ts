ec6a7790428791ea9404d104e1209b87ac823610
timc986 <timothychan986@hotmail.com>
2021-01-15 11:25:03 +0000

    add home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  // { path: 'order', component: OrderComponent },
+  // { path: 'upload', component: UploadComponent },
+  // { path: 'login', component: AppComponent },
+  // { path: 'register', component: AppComponent },
+  // { path: 'settings', component: AppComponent, canActivate: [AuthGuard] },
+  // // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
