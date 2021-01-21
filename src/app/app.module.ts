8143596bb8775d2b168a0159298cfdc71c02bd2f
timc986 <timothychan986@hotmail.com>
2021-01-21 18:08:24 +0000

    add tech and music component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TechComponent } from './tech/tech.component';
+import { MusicComponent } from './music/music.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TechComponent,
+    MusicComponent
   ],
   imports: [
     BrowserModule,
