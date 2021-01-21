f0e09f01c168932c1ae937ae00a20bd4f895c606
timc986 <timothychan986@hotmail.com>
2021-01-21 17:21:43 +0000

    remove routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  // { path: 'order', component: OrderComponent },
-  // { path: 'upload', component: UploadComponent },
-  // { path: 'login', component: AppComponent },
-  // { path: 'register', component: AppComponent },
-  // { path: 'settings', component: AppComponent, canActivate: [AuthGuard] },
-  // // otherwise redirect to home
-  { path: '**', redirectTo: '' }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
